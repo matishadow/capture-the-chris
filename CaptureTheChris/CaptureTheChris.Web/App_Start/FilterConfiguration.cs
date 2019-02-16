aebfe38ce7685fac7cf9a62bcdb9b178eb30999f
Matt Sawyer <matishadow@gmail.com>
2019-02-16 20:47:37 +0100

    Add solution template

---
+++ CaptureTheChris/CaptureTheChris.Web/App_Start/FilterConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CaptureTheChris.Web
+{
+    internal static class FilterConfiguration
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CaptureTheChris/CaptureTheChris.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaptureTheChris.Enums;
+
+namespace CaptureTheChris.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GeneratePassword()
+        {
+            return Json(null);
+        }
+    }
+}

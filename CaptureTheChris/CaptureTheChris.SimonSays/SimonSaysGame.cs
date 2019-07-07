4e0d54ab9fcce83bb7ba4d9f8335a53b619f3d1a
matishadow <matishadow@gmail.com>
2019-07-07 21:40:39 +0200

    Implement SimonSays game as IGame

--- CaptureTheChris/CaptureTheChris.SimonSays/SimonSaysGame.cs
+++ CaptureTheChris/CaptureTheChris.SimonSays/SimonSaysGame.cs
@@ -3,73 +3,64 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using CaptureTheChris.Enums;
+using CaptureTheChris.GameLogic;
 using CaptureTheChris.Interfaces.SimonSays;
 
 namespace CaptureTheChris.SimonSays
 {
-    public class SimonSaysGame 
+    public class SimonSaysGame : Game, IGame
     {
         private readonly ISimonSaysColorGenerator simonSaysColorGenerator;
+        private const int NumberOfRounds = 30;
 
         private SimonSaysColor[] simonSaysColors;
         private int roundNumber;
 
-        public SimonSaysGame(ISimonSaysColorGenerator simonSaysColorGenerator)
+        public SimonSaysGame(ISimonSaysColorGenerator simonSaysColorGenerator) 
+            : base(Flags.Properties.Resources.FlagSimonSays)
         {
             this.simonSaysColorGenerator = simonSaysColorGenerator;
         }
 
-        private bool IsGameRunning => simonSaysColors != null;
-
-        public bool StartGame(int numberOfRounds)
-        {
-            if (IsGameRunning)
-                throw new InvalidOperationException(); 
-            
-            if (numberOfRounds <= 0)
-                throw new ArgumentOutOfRangeException();
-
-            roundNumber = 1;
-            simonSaysColors = new SimonSaysColor[numberOfRounds];
-            for (var i = 0; i < simonSaysColors.Length; i++)
-                simonSaysColors[i] = simonSaysColorGenerator.GetRandomColor();
-
-            return true;
-        }
-
-        public bool StopGame()
-        {
-            if (!IsGameRunning)
-                throw new InvalidOperationException(); 
-            
-            simonSaysColors = null;
-            roundNumber = 0;
-            
-            return true;
-        }
-
         public IEnumerable<SimonSaysColor> GetColorsToRemember()
         {
-            if (!IsGameRunning)
-                throw new InvalidOperationException();
+            CheckRunningGame();
 
             return simonSaysColors.Take(roundNumber);
         }
 
-        public bool TurnRound(IList<SimonSaysColor> colors)
+        public void TurnRound(IList<SimonSaysColor> colors)
         {
-            if (!IsGameRunning)
-                throw new InvalidOperationException();
+            CheckRunningGame();
+            
             if (colors == null)
                 throw new ArgumentNullException();
             if (!colors.Any() || colors.Count > simonSaysColors.Length)
                 throw new ArgumentOutOfRangeException();
 
             if (colors.Where((gameColor, i) => gameColor != simonSaysColors[i]).Any())
-                return false;
+            {
+                IsRunning = false;
+                roundNumber = 0;
+                return;
+            }
             
             roundNumber++;
-            return true;
+            if (roundNumber > NumberOfRounds)
+                IsWon = true;
+        }
+
+        public override bool IsWon { get; protected set; }
+        public override bool IsRunning { get; protected set; }
+        public override void StartGame()
+        {
+            IsRunning = true;
+            IsWon = false;
+            roundNumber = 1;
+            
+            simonSaysColors = new SimonSaysColor[NumberOfRounds];
+            for (var i = 0; i < simonSaysColors.Length; i++)
+                simonSaysColors[i] = simonSaysColorGenerator.GetRandomColor();
         }
     }
 }

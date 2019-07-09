f93c6dc6b9a5d8b9cbfc6f6435d007e84b86b134
matishadow <matishadow@gmail.com>
2019-07-09 18:22:04 +0200

    Add controller and view

---
+++ CaptureTheChris/CaptureTheChris.Web/Controllers/ProjectEulerController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace CaptureTheChris.Web.Controllers
+{
+    public class ProjectEulerController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

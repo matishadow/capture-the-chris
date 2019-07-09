69cbb300d1f5479dc9c75d93f099661183b45b60
matishadow <matishadow@gmail.com>
2019-07-09 21:38:56 +0200

    Add post method in ProjectEulerContoller

--- CaptureTheChris/CaptureTheChris.Web/Controllers/ProjectEulerController.cs
+++ CaptureTheChris/CaptureTheChris.Web/Controllers/ProjectEulerController.cs
@@ -1,13 +1,30 @@
 using System.Web.Mvc;
+using CaptureTheChris.ProjectEuler;
 
 namespace CaptureTheChris.Web.Controllers
 {
     public class ProjectEulerController : Controller
     {
-        // GET
+        private readonly IProjectEulerGame projectEulerGame;
+
+        public ProjectEulerController(IProjectEulerGame projectEulerGame)
+        {
+            this.projectEulerGame = projectEulerGame;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(projectEulerGame);
+        }
+
+        [HttpPost]
+        public ActionResult Answer(string answer)
+        {
+            projectEulerGame.StartGame();
+            
+            projectEulerGame.ProvideAnswer(answer);
+
+            return View("Index", projectEulerGame);
         }
     }
 }

ca32895c1886457a8d276a7252ddb8d382337e07
matishadow <matishadow@gmail.com>
2019-07-13 20:56:43 +0200

    Add Sudoku controller

---
+++ CaptureTheChris/CaptureTheChris.Web/Controllers/SudokuController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CaptureTheChris.Sudoku;
+using CaptureTheChris.Web.Models;
+
+namespace CaptureTheChris.Web.Controllers
+{
+    public class SudokuController : Controller
+    {
+        private readonly ISudokuGame sudokuGame;
+
+        public SudokuController(ISudokuGame sudokuGame)
+        {
+            this.sudokuGame = sudokuGame;
+        }
+
+        public ActionResult Index()
+        {
+            return View(sudokuGame);
+        }
+
+        [HttpPost]
+        public PartialViewResult Answer(IEnumerable<int> answer)
+        {
+            sudokuGame.StartGame();
+            
+            bool wasLastTrySuccessful = sudokuGame.TryProvideAnswer(answer);
+            var gameResult = new GameResult(sudokuGame.IsWon, sudokuGame.GetFlag(), wasLastTrySuccessful);
+
+            return PartialView("_Flag", gameResult);
+        }
+    }
+}

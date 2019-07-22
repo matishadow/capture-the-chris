7c22531ec7962e54a4a738e87196bed4252f8efb
matishadow <matishadow@gmail.com>
2019-07-22 21:07:43 +0200

    Make random colors distinct, add running game check

--- CaptureTheChris/CaptureTheChris.Enigma/EnigmaGame.cs
+++ CaptureTheChris/CaptureTheChris.Enigma/EnigmaGame.cs
@@ -13,7 +13,7 @@ namespace CaptureTheChris.Enigma
         ISingleInstanceDependency, IAsImplementedInterfacesDependency
     {
         private const int MaxNumberOfTries = 6; // might need to be tweaked to 5
-        private readonly EnigmaField[] enigmaFields = new EnigmaField[4];
+        private EnigmaField[] enigmaFields = new EnigmaField[4];
         public IList<EnigmaGuessResult> Tries { get; } = new List<EnigmaGuessResult>();
 
         private readonly IRandomColorGenerator randomColorGenerator;
@@ -27,50 +27,61 @@ namespace CaptureTheChris.Enigma
 
         public override bool IsWon { get; protected set; }
         public override bool IsRunning { get; protected set; }
+
         public override void StartGame()
         {
             IsWon = false;
             IsRunning = true;
-            
+
             Tries.Clear();
+            enigmaFields = new EnigmaField[4];
             SetRandomColors();
             ResetTries();
         }
 
         public EnigmaGuessResult GuessColors(EnigmaColor[] colors)
         {
-           var guessResult = new EnigmaGuessResult();
-           
-           if (colors == null || colors.Length != enigmaFields.Length)
-               throw new ArgumentException($"Input must be of size {enigmaFields.Length}");
+            CheckRunningGame();
+            var guessResult = new EnigmaGuessResult();
 
-           for (var i = 0; i < enigmaFields.Length; i++)
-           {
-               guessResult.tries[i] = new EnigmaField(colors[i], enigmaFields[i].Guess(colors[i]));
-           } 
-           Tries.Add(guessResult);
-           
-           if (AllColorsGuessed())
-           {
-               IsRunning = false;
-               IsWon = true;
-           }
-           else
-           {
-               CurrentNumberOfTries -= 1;
+            if (colors == null || colors.Length != enigmaFields.Length)
+                throw new ArgumentException($"Input must be of size {enigmaFields.Length}");
 
-               if (CurrentNumberOfTries >= 1) return guessResult;
-               IsRunning = false;
-               IsWon = false;
-           }
+            for (var i = 0; i < enigmaFields.Length; i++)
+            {
+                guessResult.tries[i] = new EnigmaField(colors[i], enigmaFields[i].Guess(colors[i]));
+            }
 
-           return guessResult;
+            Tries.Add(guessResult);
+
+            if (AllColorsGuessed())
+            {
+                IsRunning = false;
+                IsWon = true;
+            }
+            else
+            {
+                CurrentNumberOfTries -= 1;
+
+                if (CurrentNumberOfTries >= 1) return guessResult;
+                IsRunning = false;
+                IsWon = false;
+            }
+
+            return guessResult;
         }
 
         private void SetRandomColors()
         {
             for (var i = 0; i < enigmaFields.Length; i++)
-                enigmaFields[i] = new EnigmaField(randomColorGenerator.GetRandomColor<EnigmaColor>());
+            {
+                var randomColor = randomColorGenerator.GetRandomColor<EnigmaColor>();
+
+                while (enigmaFields.Any(field => field != null && field.EnigmaColor == randomColor))
+                    randomColor = randomColorGenerator.GetRandomColor<EnigmaColor>();
+
+                enigmaFields[i] = new EnigmaField(randomColor);
+            }
         }
 
         private void ResetTries()

5cacc38f0a291a0008aaddc2c12ce7b96c0230b4
matishadow <matishadow@gmail.com>
2019-07-26 22:10:49 +0200

    Add outside controllers

---
+++ CaptureTheChris/CaptureTheChris.Web/Controllers/CakeController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using CaptureTheChris.Enigma;
+using CaptureTheChris.Enums;
+using CaptureTheChris.Hangman;
+using CaptureTheChris.Time;
+using CaptureTheChris.Web.Attributes;
+using CaptureTheChris.Web.Models;
+
+namespace CaptureTheChris.Web.Controllers
+{
+    [CakeTaskFilter]
+    public class CakeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
